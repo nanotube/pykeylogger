6f35028617a2397a128780855f9b763b12d18de3
nanotube <nanotube>
2005-09-05 15:16:19 +0000

    adding this file for freezing windows executables

---
+++ setup.py
@@ -0,0 +1,23 @@
+# A very simple setup script to create an executable.
+#
+# Run the build process by entering 'setup.py py2exe' or
+# 'python setup.py py2exe' in a console prompt.
+#
+# If everything works well, you should find a subdirectory named 'dist'
+# containing some files, among them hello.exe and test_wx.exe.
+
+
+from distutils.core import setup
+import py2exe
+
+setup(
+    # The first three parameters are not required, if at least a
+    # 'version' is given, then a versioninfo resource is built from
+    # them and added to the executables.
+    version = "0.2.1",
+    description = "simple python keylogger",
+    name = "python keylogger",
+
+    # targets to build
+    console = ["keylogger.py"],
+    )
