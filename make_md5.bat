8d4c684822fe9c0da4a15fc5a181ae9a3a8d6b73
nanotube <nanotube>
2005-09-11 20:34:18 +0000

    add md5sum maker for dist packages.

---
+++ make_md5.bat
@@ -0,0 +1,10 @@
+rem *** Used to create a Python exe  
+
+setlocal
+set VERSION=0.4.1
+
+"C:\Program Files\UnixUtils\md5sum.exe" "../pykeylogger%VERSION%_src.zip" "../pykeylogger%VERSION%_win32.zip" > "../pykeylogger%VERSION%_md5sums.txt"
+
+endlocal
+
+pause "done, press to key to exit"
