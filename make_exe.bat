07169c97e4402c9704b0aaa727a824033e771818
nanotube <nanotube>
2005-09-11 17:55:47 +0000

    make bats more functional, to create releases automatically.

--- make_exe.bat
+++ make_exe.bat
@@ -1,5 +1,8 @@
 rem *** Used to create a Python exe  
- 
+
+setlocal
+set VERSION=0.4
+
 rem ***** get rid of all the old files in the build folder 
 rd /S /Q build 
  
@@ -7,4 +10,22 @@ rem ***** create the exe
 c:\Python24\python setup.py py2exe 
  
 rem **** pause so we can see the exit codes 
-pause "done...hit a key to exit" 
+pause "done, press key to continue"
+
+rem **** rename dist directory to 'pykeylogger'
+rename "dist" "pykeylogger"
+
+echo %VERSION%
+pause "..."
+
+"C:\Program Files\WinRAR\WinRAR.exe" a -r "pykeylogger%VERSION%_win32.zip" "pykeylogger"
+
+pause "lets inspect the filename"
+
+mv "./pykeylogger%VERSION%_win32.zip" "../pykeylogger%VERSION%_win32.zip"
+
+rd /S /Q pykeylogger
+
+endlocal
+
+pause "done, press to key to exit"
---
+++ make_src_dist.bat
@@ -0,0 +1,16 @@
+rem *** Used to create a Python exe  
+
+setlocal
+set VERSION=0.4
+
+"C:\Program Files\WinRAR\WinRAR.exe" a -r "pykeylogger%VERSION%_src.zip" "keylogger.pyw" "logwriter.py" "setup.py" "*.txt" "*.bat" "html"
+
+pause "lets inspect the filename"
+
+mv "./pykeylogger%VERSION%_src.zip" "../pykeylogger%VERSION%_src.zip"
+
+rd /S /Q pykeylogger
+
+endlocal
+
+pause "done, press to key to exit"
