91e15873c0a6854b9e3815c298627499054997ed
nanotube <nanotube>
2005-09-21 05:08:16 +0000

    a bat file to run pykeylogger with your cmdline options. useful for putting in startup.

---
+++ run_pykeylogger_with_cmdoptions.bat
@@ -0,0 +1,35 @@
+::usage: keylogger.pyw [options]
+::
+::options:
+::  --version             show program's version number and exit
+::  -h, --help            show this help message and exit
+::  -f DIRNAME, --file=DIRNAME
+::                        write log data to DIRNAME [default: C:\Temp\logdir]
+::  -k, --keyboard        log keyboard input [default: True]
+::  -a, --addlinefeed     add linefeed [\n] character when carriage return [\r]
+::                        character is detected (for Notepad compatibility)
+::                        [default: False]
+::  -b, --parsebackspace  translate backspace chacarter into printable string
+::                        [default: False]
+::  -e, --parseescape     translate escape chacarter into printable string
+::                        [default: False]
+::  -x EXITKEY, --exitkey=EXITKEY
+::                        specify the key to press to exit keylogger [default:
+::                        F12]
+::  -l FLUSHKEY, --flushkey=FLUSHKEY
+::                        specify the key to press to flush write buffer to file
+::                        [default: F11]
+::  -d, --debug           debug mode (print output to console instead of the log
+::                        file) [default: False]
+::  -n NOLOG, --nolog=NOLOG
+::                        specify an application by full path name whose input
+::                        will not be logged. repeat option for multiple
+::                        applications. [default: none]
+::  -o ONEFILE, --onefile=ONEFILE
+::                        log all output to one file ONEFILE, (inside DIRNAME,
+::                        as specified with -f option), rather than to multiple
+::                        files. [default: none]
+
+@echo off
+
+start c:\Python24\pythonw.exe keylogger.pyw -n "C:\Program Files\Gaim\gaim.exe"
