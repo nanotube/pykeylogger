fd0ac0b1dca2c23052a879cec6a86a7eee8c2462
nanotube <nanotube>
2008-07-31 13:04:40 +0000

    reorder, for clarity

--- version.py
+++ version.py
@@ -1,7 +1,10 @@
 
-name = "pykeylogger" # change this for stealth. you must also change the filenames of the .ini, .val, and the icon files and replace "pykeylogger" with what you put in here.
+# change this for stealth. you must also change the filenames of the .ini, .val, and the icon files and replace "pykeylogger" with what you put in here.
+name = "pykeylogger"
+# change this for stealth
+description = "Simple Python Keylogger"
+
 version = "1.0.3"
-description = "Simple Python Keylogger" # change this for stealth
 url = "http://pykeylogger.sourceforge.net"
 license = "GPL"
 author = "Daniel Folkinshteyn"
