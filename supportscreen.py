8fdae77dff8ab0ca02b2955f84d4b7646382a328
Gra <gra@opame.net>
2009-04-24 14:24:58 +0200

    Factorised an URL, reworked text of support and expiration screens (79 cols).

--- supportscreen.py
+++ supportscreen.py
@@ -5,6 +5,9 @@ import ScrolledText
 import version
 
 class SupportScreen(mytkSimpleDialog.Dialog):
+    
+    BASE_SF_URL = "http://pykeylogger.wiki.sourceforge.net"
+    
     def __init__(self, parent, title = None,
                  rootx_offset=50, rooty_offset=50):
         mytkSimpleDialog.Dialog.__init__(self, parent, title,
@@ -19,27 +22,40 @@ class SupportScreen(mytkSimpleDialog.Dialog):
         self.t.tag_bind("href", "<Enter>", self.show_hand_cursor)
         self.t.tag_bind("href", "<Leave>", self.show_arrow_cursor)
         self.t.config(cursor="arrow", bg="white", wrap=WORD)
-        self.t.insert(END, "Welcome to PyKeylogger, a versatile backup and system monitoring solution. \n\n\
-PyKeylogger is Free Open Source Software, licensed under the Gnu General Public License. \
-You can download the source code from ")
-        self.t.insert(END, "http://sourceforge.net/projects/pykeylogger", "href")
-        self.t.insert(END, "\n\nHit 'Lcontrol-Rcontrol-F12' to bring up the Control Panel (default password is blank). More help is in the Help menu.\n\n\
-Since I am but a poor grad student, \
-you are strongly encouraged to donate to this open source project. \
-So strongly, in fact, that this program is limited to 4 days of use, and presents you with this nag \
-screen every time you start it. \
-There is good logic behind why this is being done. If you are curious, you will find the following \
-link very informative: ")
+        self.t.insert(END,
+           "Welcome to PyKeylogger, a versatile backup and system monitoring "
+           "solution. \n\n"
+           "PyKeylogger is Free Open Source Software, licensed under the "
+           "Gnu General Public License. "
+           "You can download the source code from ")
+        self.t.insert(END,
+           "http://sourceforge.net/projects/pykeylogger", "href")
+        self.t.insert(END,
+           "\n\nHit 'Lcontrol-Rcontrol-F12' to bring up the Control Panel "
+           "(default password is blank). More help is in the Help menu.\n\n"
+           "Since I am but a poor grad student, you are strongly encouraged "
+           "to donate to this open source project. So strongly, in fact, that "
+           "this program is limited to 4 days of use, and presents you with "
+           "this nag screen every time you start it. There is good logic "
+           "behind why this is being done. If you are curious, you will find "
+           "the following link very informative: ")
         self.t.insert(END, "http://hackvan.com/pub/stig/articles/why-do-people-register-shareware.html", "href")
-        self.t.insert(END, "\n\nThere are two ways to get rid of the nag and expiration: \n \
-1. Donate to PyKeylogger by following the simple instructions at ")
-        self.t.insert(END, "http://pykeylogger.wiki.sourceforge.net/Download_Instructions", "href")
-        self.t.insert(END, " and you will get a binary build of PyKeylogger without any nagging, \
-by E-mail, HTTP, or FTP.")
-        self.t.insert(END, "\n\n 2. Get the project source code, the supporting libraries, then find and toggle the nag control. You can then run \
-PyKeylogger from source, or even build your own executable. Detailed instructions for this approach are available at ")
-        self.t.insert(END, "http://pykeylogger.wiki.sourceforge.net/Installation_Instructions", "href")
-        self.t.insert(END, "\n\nFinally, I encourage you to use this software responsibly, keeping to the law and your own moral code.")
+        self.t.insert(END,
+           "\n\nThere are two ways to get rid of the nag and expiration: \n"
+           "1. Donate to PyKeylogger by following the simple instructions at ")
+        self.t.insert(END, BASE_SF_URL + "/Download_Instructions", "href")
+        self.t.insert(END,
+           " and you will get a binary build of PyKeylogger without any "
+           "nagging, by E-mail, HTTP, or FTP.")
+        self.t.insert(END,
+           "\n\n 2. Get the project source code, the supporting libraries, "
+           "then find and toggle the nag control. You can then run "
+           "PyKeylogger from source, or even build your own executable. "
+           "Detailed instructions for this approach are available at ")
+        self.t.insert(END, BASE_SF_URL + "/Installation_Instructions", "href")
+        self.t.insert(END,
+           "\n\nFinally, I encourage you to use this software responsibly, "
+           "keeping to the law and your own moral code.")
         self.t.config(state=DISABLED)
 
     def show_hand_cursor(self, event):
@@ -83,19 +99,31 @@ class ExpirationScreen(mytkSimpleDialog.Dialog):
         self.t.tag_bind("href", "<Enter>", self.show_hand_cursor)
         self.t.tag_bind("href", "<Leave>", self.show_arrow_cursor)
         self.t.config(cursor="arrow", bg="white", wrap=WORD)
-        self.t.insert(END, "Thank you for using PyKeylogger, a versatile backup and system monitoring solution.")
-        self.t.insert(END, "\n\nAs you may remember from reading the \"welcome screen\", this binary expires \
-after 4 days of use, as a method of encouraging donations to this open source software project. This installation of \
-PyKeylogger has now *EXPIRED*. There are two ways \
-to restore PyKeylogger's functionality: \n\n 1. Donate to PyKeylogger by following the simple instructions at ")
-        self.t.insert(END, "http://pykeylogger.wiki.sourceforge.net/Download_Instructions", "href")
-        self.t.insert(END, " and you will get a binary build of PyKeylogger without any nagscreens or expiration, \
-by E-mail, HTTP, or FTP.")
-        self.t.insert(END, "\n\n 2. Get the project source code, the supporting libraries, then find and toggle the nag control. You can then run \
-PyKeylogger from source, or even build your own executable. Detailed instructions for this approach are available at ")
-        self.t.insert(END, "http://pykeylogger.wiki.sourceforge.net/Installation_Instructions", "href")
-        self.t.insert(END, "\n\nIf you run into any trouble, feel free to ask for help on the PyKeylogger forums: ")
-        self.t.insert(END, "http://sourceforge.net/forum/?group_id=147501", "href")
+        self.t.insert(END,
+           "Thank you for using PyKeylogger, a versatile backup and system "
+           "monitoring solution.")
+        self.t.insert(END,
+           "\n\nAs you may remember from reading the \"welcome screen\", "
+           "this binary expires after 4 days of use, as a method of "
+           "encouraging donations to this open source software project. "
+           "This installation of PyKeylogger has now *EXPIRED*. There are two "
+           "ways to restore PyKeylogger's functionality: \n\n "
+           "1. Donate to PyKeylogger by following the simple instructions at ")
+        self.t.insert(END, BASE_SF_URL + "/Download_Instructions", "href")
+        self.t.insert(END,
+           " and you will get a binary build of PyKeylogger without any "
+           "nagscreens or expiration, by E-mail, HTTP, or FTP.")
+        self.t.insert(END,
+           "\n\n 2. Get the project source code, the supporting libraries, "
+           "then find and toggle the nag control. You can then run "
+           "PyKeylogger from source, or even build your own executable. "
+           "Detailed instructions for this approach are available at ")
+        self.t.insert(END, BASE_SF_URL + "/Installation_Instructions", "href")
+        self.t.insert(END,
+           "\n\nIf you run into any trouble, feel free to ask for help on the "
+           "PyKeylogger forums: ")
+        self.t.insert(END,
+           "http://sourceforge.net/forum/?group_id=147501", "href")
         self.t.config(state=DISABLED)
 
     def show_hand_cursor(self, event):
