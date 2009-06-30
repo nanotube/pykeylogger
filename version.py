e11fe0767a5f11e1a6067119ab338f930c37b12e
Daniel Folkinshteyn <nanotube@users.sourceforge.net>
2009-06-30 00:05:47 -0400

    update version for 1.0.5

--- version.py
+++ version.py
@@ -5,7 +5,7 @@ name = "pykeylogger"
 # Change this for stealth.
 description = "Simple Python Keylogger"
 
-version = "1.0.4"
+version = "1.0.5"
 url = "http://pykeylogger.sourceforge.net"
 license = "GPL"
 author = "Daniel Folkinshteyn"
